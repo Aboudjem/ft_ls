c88bcdca1ca61e1171f5051d9098333ce0e5aefb
Adx <adx@MacBook-Pro-de-Adx.local>
2017-05-01 23:49:08 +0200

    Push laki

--- src/ft_ls.c
+++ src/ft_ls.c
@@ -7,7 +7,7 @@ size_t	ft_get_size(t_ls *tmp)
 	i = 0;
 	while(tmp)
 	{
-		if (tmp->data.size > i)
+		if ((size_t)tmp->data.size > i)
 			i = tmp->data.size;
 		tmp = tmp->next;
 	}
@@ -17,10 +17,10 @@ return(i);
 void 	print_lst(t_ls *lst, t_flags f)
 {
 	t_ls *tmp;
-	size_t len_size;
+	size_t i;
 	tmp = lst;
 
-	len_size = ft_get_size(tmp);
+	i = ft_get_size(tmp);
 
 	if (CHECK_BIT(f.bit, R))
 		sort_reverse(tmp);
@@ -31,17 +31,17 @@ void 	print_lst(t_ls *lst, t_flags f)
 		sort_list_time(tmp);
 	while (tmp)
 	{
-		// if (!(CHECK_BIT(f.bit, A)) && tmp->data.name[0] == '.')
-		// 	tmp = tmp->next;
-		// else
-		// {
-		// 	if (!(CHECK_BIT(f.bit, L)))
-		// 		ft_putstr(tmp->data.name);
-		// 	else
+		if (!(CHECK_BIT(f.bit, A)) && tmp->data.name[0] == '.')
+			tmp = tmp->next;
+		else
+		{
+			if (!(CHECK_BIT(f.bit, L)))
+				ft_putstr(tmp->data.name);
+			else
 				print_full(tmp->data);
 				ft_putstr("\n");
 				tmp = tmp->next;
-		// }
+		}
 	}
 }
 
