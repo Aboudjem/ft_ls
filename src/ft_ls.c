830c468ba7f190f7acc08495c71b57f3e0ecb4ac
Adam BOUDJEMAA <aboudjem@e2r3p12.42.fr>
2017-06-29 01:46:46 +0200

    push

--- src/ft_ls.c
+++ src/ft_ls.c
@@ -35,59 +35,119 @@ void		ls_dir(char *d, t_flags f)
 		exit(-1);
 }
 
-void		stock_r(t_ls *lst, t_flags f, char *d)
-{
-	DIR *dir;
-	char *path;
-	if (!(dir = opendir(d)))
-		exit (0);
-	t_dir *file;
-	if (dir == NULL)
-		exit(0);
-	while ((file = readdir(dir)))
-	{
-		if ((get_type(file->d_type) == 'd'))
-		{
-			if ((ft_strcmp(file->d_name, "..")) && (ft_strcmp(file->d_name, ".")))
-			{
+// void		stock_r(t_ls *lst, t_flags f, char *d)
+// {
+// 	DIR *dir;
+// 	char *path;
+// 	if (!(dir = opendir(d)))
+// 		exit (0);
+// 	t_dir *file;
+// 	if (dir == NULL)
+// 		exit(0);
+// 	while ((file = readdir(dir)))
+// 	{
+// 		if ((get_type(file->d_type) == 'd'))
+// 		{
+// 			if ((ft_strcmp(file->d_name, "..")) && (ft_strcmp(file->d_name, ".")))
+// 			{
+// 			// closedir(d);
+// 			path = ft_strjoin(d, "/");
+// 			path = ft_strjoin(path, file->d_name);
+// 			ft_printf("[%s]", path);
+// 			stock_r(lst, f, path);
+// 			}
+// 		}
+// 		// ft_putstr(file->d_name);
+// 		// ft_putstr("\n");
+// 	}
+// 		ft_putstr("\n");
 
-			path = ft_strjoin(d, "/");
-			path = ft_strjoin(path, file->d_name);
-			ft_printf("[%s]", path);
-			stock_r(lst, f, path);
-			}
-		}
-		ft_putstr(file->d_name);
-		ft_putstr("\n");
-	}
-		ft_putstr("\n");
+// }
+
+// void		ls_r(t_flags f, char *d)
+// {
+// 	t_ls *lst;
+// 	if(!(lst = (t_ls*)ft_memalloc(sizeof(t_ls))))
+// 		exit (0);
+// 	DIR *dir;
+// 	dir = opendir(d);
+// 	t_dir *file;
+
+// 	file = NULL;
+// 	while ((file = readdir(dir)))
+// 	{
+// 		if ((get_type(file->d_type) == 'd'))
+// 			if ((ft_strcmp(file->d_name, "..")) && (ft_strcmp(file->d_name, ".")))
+// 					{
+// 						ft_printf("%s/%s:\n", d, file->d_name);
+// 						stock_r(lst, f, file->d_name);
+// 						closedir(file->d_name);
+// 					}
+// 	}
+// }
+
+			// if ((ft_strcmp(file->d_name, "..")) && (ft_strcmp(file->d_name, ".")))
+
+char 	*rec_dir(char *d, char *new)
+{
+	char *tmp;
+
+	tmp = ft_strjoin(d, "/");
+	tmp = ft_strjoin(tmp, new);
+	ft_printf("---------- %s ---------\n", tmp);
+	return(tmp);
 
 }
 
-void		ls_r(t_flags f, char *d)
+int 	check_dir(t_dir *file, char *d)
+{
+
+	if (get_type(file->d_type) == 'd' && (ft_strcmp(file->d_name, "..") && (ft_strcmp(file->d_name, "."))))
+					return(1);
+					// ft_printf("[%s]\n", file->d_name);
+	return(0);
+}
+
+static void		display_r(t_flags f, t_dir *file, char *d)
+{
+(void)f;
+	DIR *dir;
+	// d = ft_strjoin(d, "/");
+	// d = ft_strjoin(d, file->d_name);
+	// ft_printf("[[[[[[[%s]]]]]]]\n\n", d);
+	if (dir = opendir(d))
+	{
+while ((file = readdir(dir)))
+{
+	if (check_dir(file, d))
+	{
+		ft_printf("[%s]\n", file->d_name);
+		display_r(f, file, rec_dir(d, file->d_name));
+	}
+}
+	// ft_printf("-> [ %d ] <-\n", check_dir(file, d));
+	// while(())
+}
+
+}
+
+static void		ls_r(t_flags f, char *d)
 {
 	t_ls *lst;
-	if(!(lst = (t_ls*)ft_memalloc(sizeof(t_ls))))
-		exit (0);
-	DIR *dir;
-	dir = opendir(d);
+	DIR  *dir;
 	t_dir *file;
-
-	file = NULL;
-	while ((file = readdir(dir)))
-	{
-		if ((get_type(file->d_type) == 'd'))
-			if ((ft_strcmp(file->d_name, "..")) && (ft_strcmp(file->d_name, ".")))
-					{
-						ft_printf("%s/%s:\n", d, file->d_name);
-						stock_r(lst, f, file->d_name);
-					}
-
-	}
-	// print_lst(lst, f);
+	if (!(lst = (t_ls*)ft_memalloc(sizeof(t_ls))))
+		exit(0);
+	if (dir = opendir(d))
+				display_r(f, file, d);// ft_printf("[%s] \n", file->d_name);
+	// while ((file = readdir(dir)))
+		// {
+			// if (get_type(file->d_type) == 'd')
+		// }
+		closedir(dir);
 }
 
-int			main(int argc, char *argv[])
+int			main()//int argc, char *argv[])
 {
 	int i;
 	t_flags f;
